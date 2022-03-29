6972adfaa2d95b44c5c11f94dcdb1505113c1b80
yeonkkk <tjddus0302@gmail.com>
2022-03-30 01:43:46 +0900

    java 기초 100제 코드

---
+++ 01.java100/Java100_variable_001.java
@@ -0,0 +1,22 @@
+package variable;
+
+public class Java100_variable_001 {
+    public static void main(String[] args){
+
+        int a;  //--- 정수가 저장될 변수 a 선언--;;
+        int b;
+        int sum;
+
+        a = 3;
+        b = 4;
+        sum = a + b;
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(sum);
+
+        sum = a + b + a;
+        System.out.println(sum);
+
+    }
+}
---
+++ 01.java100/Java100_variable_HelloWorld.java
@@ -0,0 +1,7 @@
+package variable;
+
+public class Java100_variable_HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World~");
+    }
+}
