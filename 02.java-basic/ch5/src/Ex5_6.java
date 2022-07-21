f1e7c3a2cd295d7bea2e7bff01691146a3576c75
yeonkkk <tjddus0302@gmail.com>
2022-07-21 17:10:27 +0900

    자바의 정석 기초편 ch5 예제 코드

---
+++ 02.java-basic/ch5/src/Ex5_6.java
@@ -0,0 +1,15 @@
+public class Ex5_6 {
+    public static void main(String[] args) {
+        String[] names = {"Kim", "Park", "Yi"};
+
+        for (int i = 0; i < names.length; i++) {
+            System.out.println("names[" + i + "]: " + names[i]);
+        }
+
+        String tmp = names[2];
+        System.out.println("tmp = " + tmp);
+        names[0] = "Yu";
+
+        for (int i = 0; i < names.length; i++) System.out.println(names[i]);
+    }
+}
