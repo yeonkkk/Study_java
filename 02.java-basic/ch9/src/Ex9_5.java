a6d7efd18bb16d7c6b96df7d0d03904d774143a5
yeonkkk <tjddus0302@gmail.com>
2022-07-28 20:51:26 +0900

    자바의 정석 기초편 ch9 예제 코드

---
+++ 02.java-basic/ch9/src/Ex9_5.java
@@ -0,0 +1,27 @@
+class Card2 {
+    String kind;
+    int number;
+
+    Card2() {
+        this("SPADE", 1);
+    }
+
+    public Card2(String kind, int number) {
+        this.kind = kind;
+        this.number = number;
+    }
+
+    public String toString() {
+        return "kind: " + kind + ", number: " + number;
+    }
+}
+
+public class Ex9_5 {
+    public static void main(String[] args) {
+        Card2 c1 = new Card2();
+        Card2 c2 = new Card2("HEART", 10);
+
+        System.out.println(c1.toString());
+        System.out.println(c2.toString());
+    }
+}
---
+++ 02.java-basic/ch9/src/Ex9_7.java
@@ -0,0 +1,8 @@
+public class Ex9_7 {
+    public static void main(String[] args) {
+        String s1 = "AAA";
+        String s2 = "AAA";
+        String s3 = "AAA";
+        String s4 = "BBB";
+    }
+}
